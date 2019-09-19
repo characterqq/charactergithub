85774542c4955c4f78287802009307af15ff308b
characterqq <945601150@qq.com>
2019-09-19 22:21:51 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -40,4 +40,56 @@ $(function(){
         })
     }
 
+
+//    2.点击添加分类按钮，
+    $('#addBtn').click(function(){
+        console.log(222);
+        $('#addModal').modal("show");
+    });
+
+//    3.调用表单效验插件，实现表单插件
+    $('#form').bootstrapValidator({
+    //    配置图标
+        feedbackIcons: {
+            valid: 'glyphicon  glyphicon-heart',  //效验成功
+            invalid: 'glyphicon glyphicon-remove' , //效验失败'
+            validating: 'glyphicon glyphicon-refresh'   //效验中
+        },
+
+    //   配置字段
+
+        fields: {
+            categoryName: {
+                validators: {
+                    notEmpty: {
+                        message: "一级分类不能为空"
+                    }
+                }
+            }
+        }
+    })
+
+//    4.注册表单效验成功事件，阻止默认的成功提交，通过ajax 进行提交
+    $('#form').on("success.form.bv", function( e ){
+        e.preventDefault();
+
+    //    通过ajax进行提交
+        $.ajax({
+            type: "post",
+            url: "/category/addTopCategory",
+            data: $('#form').serialize(),
+            success: function( info ){
+                if( info.success ){
+                //    添加成功
+                //    1.关闭模态框
+                    $('#addModal').modal("hide");
+                //    2.页面重新渲染
+                    currentPage = 1;
+                    render();
+                //    3.重置模态框
+                    $('#form').data("bootstrapValidator").resetForm(true);
+                }
+            }
+        })
+    })
 })
